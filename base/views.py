f561af1f1abf996aced63934ea87c6aeb0f0045e
ISHEJA Ladouce <ishedouce2002@gmail.com>
2025-03-17 10:25:43 -0700

    Add necessary pages

--- base/views.py
+++ base/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def contact(request):
+    return render(request, 'contact.html')
